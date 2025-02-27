60c524b43436b462948e9a65f3a3e4a84c2d596e
Sudarshanyadav111 <ksudarshanyadav111@gmail.com>
2025-02-27 10:33:30 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,12 @@
+
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+  var dots=elem.getAttribute("data-dots");
+  var marked=elem.getAttribute("data-percent");
+  var percent = Math.floor(dots*marked/100);
+  var points = "";
+  var rotate = 360/dots;
+  for(let i=0;i<dots;i++){
+    points+='<div class="points" style="--i:${i}; --rot:${rotate}deg"</div>';}
+    elem.innerHtml=points;
+})
